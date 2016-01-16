da2962f53c4826c5c119772f770bd87fa864c82f
Ian Ibbotson <ian.ibbotson@gmail.com>
2016-01-16 13:41:29 +0000

    Added agent cannibalised from AirQuality+ to virtuoso datafeed

---
+++ agent/ial.groovy
@@ -0,0 +1,38 @@
+@Grapes([
+    // @GrabResolver(name='central', root='http://central.maven.org/maven2/'),
+    @GrabResolver(name='mvnRepository', root='http://central.maven.org/maven2/'),
+    @Grab(group='org.slf4j', module='slf4j-api', version='1.7.6'),
+    @Grab(group='org.slf4j', module='jcl-over-slf4j', version='1.7.6'),
+    @Grab(group='net.sourceforge.nekohtml', module='nekohtml', version='1.9.14'),
+    @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.1'),
+    @Grab(group='xerces', module='xercesImpl', version='2.9.1')
+])
+
+// http://www.sheffieldairmap.org/view_map.html
+// http://uk-air.defra.gov.uk/networks/site-info?site_id=SHE2
+
+import groovyx.net.http.*
+import static groovyx.net.http.ContentType.URLENC
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
+import groovyx.net.http.*
+import org.apache.http.entity.mime.*
+import org.apache.http.entity.mime.content.*
+import java.nio.charset.Charset
+import static groovy.json.JsonOutput.*
+import java.text.SimpleDateFormat
+
+
+
+doUpdate('https://alerts.internetalerts.org/feed')
+
+
+
+def doUpdate(baseurl) {
+  try {
+    def rss_page = new XmlParser( new org.cyberneko.html.parsers.SAXParser() ).parse(baseurl) 
+  }
+  catch ( Exception e ) {
+    println("ERROR....(${baseurl})"+e.message);
+  }
+}
