1b3b8fe1e83f8f9c640ee77ba40fbd83469cfb54
Ian Ibbotson <ian.ibbotson@gmail.com>
2016-01-23 12:34:52 +0000

    Working on bootstrap test cases

---
+++ CAPAlertsService/grails-app/domain/capalerts/AlertProfile.groovy
@@ -0,0 +1,12 @@
+package capalerts
+
+class AlertProfile {
+
+  String name
+  String shapeType
+  String shapeCoordinates
+  String radius
+
+  static constraints = {
+  }
+}
--- CAPAlertsService/grails-app/init/BootStrap.groovy
+++ CAPAlertsService/grails-app/init/BootStrap.groovy
@@ -1,7 +1,24 @@
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+  def init = { servletContext ->
+
+    assertProfile('Flagstaff Co. near Killam and Sedgewick',
+                  'polygon',
+                  '[ [ [ -111.5995, 52.9305 ], [ -111.876, 52.6676 ], [ -111.9873, 52.9598 ], [ -111.8906, 52.9597 ], [ -111.5995, 52.9305 ] ] ]');
+
+    assertProfile('Near Sheffield, UK',
+                  'circle',
+                  '[ -1.466944, 53.383611 ]',
+                  '5000m'); // 5km circle
+  }
+
+  def destroy = {
+  }
+
+  def assertProfile(name, type, shape, radius=null) {
+    def p = AlertProfile.findByName(name) ?: new AlertProfile(name:name, 
+                                                              shapeType:type, 
+                                                              shapeCoordinates:shape,
+                                                              radius:radius).save(flush:true, failOnError:true);
+  }
 }
