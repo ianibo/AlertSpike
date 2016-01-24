f5f98a06cd9143280dacea1526f5bbf7682cb89d
Ian Ibbotson <ianibbo@gmail.com>
2016-01-24 16:31:36 +0000

    Work on feed endpoint

--- CAPAlertsService/grails-app/controllers/capalerts/ProfileController.groovy
+++ CAPAlertsService/grails-app/controllers/capalerts/ProfileController.groovy
@@ -1,10 +1,29 @@
 package capalerts
 
+import grails.converters.JSON
+
 class ProfileController {
 
   def index() { 
+    def result = [:]
+    result.alerts = AlertProfile.executeQuery('select ap from AlertProfile as ap where ap.name like ?',['%'], [max: 10, offset: 0]);
+    render result as JSON
   }
 
   def feed() {
+
+    def result = [:]
+
+    if ( params.id ) {
+      result.alert = AlertProfile.get(params.id)
+      if ( result.alert) {
+      }
+      else {
+        result.message = "Unable to locate profile with id "+params.id;
+      }
+    }
+
+    render result as JSON
   }
+
 }
