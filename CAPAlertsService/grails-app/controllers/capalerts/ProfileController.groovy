59f26338fcb11853815b6243b7c7a6d1ebb790a3
Ian Ibbotson <ianibbo@gmail.com>
2016-01-24 21:22:39 +0000

    Wiring in search to feed endpoint2

--- CAPAlertsService/grails-app/controllers/capalerts/ProfileController.groovy
+++ CAPAlertsService/grails-app/controllers/capalerts/ProfileController.groovy
@@ -4,6 +4,8 @@ import grails.converters.JSON
 
 class ProfileController {
 
+  def ESWrapperService
+
   def index() { 
     def result = [:]
     result.alerts = AlertProfile.executeQuery('select ap from AlertProfile as ap where ap.name like ?',['%'], [max: 10, offset: 0]);
@@ -16,10 +18,44 @@ class ProfileController {
   def feed() {
 
     def result = [:]
+    result.offset = 0;
+    result.max = 10;
 
-    if ( params.id ) {
+    def esclient = ESWrapperService.getClient();
+
+    if ( ( params.id ) && ( esclient ) ) {
       result.alert = AlertProfile.get(params.id)
-      if ( result.alert) {
+      if ( result.alert ) {
+
+        def query_string="q:*"
+
+        try {
+
+          def search = esclient.search {
+                       indices 'alerts'
+                       types 'alert'
+                       source {
+                         from = result.offset
+                         size = result.max
+                         query {
+                           query_string (query: query_str)
+                         }
+                         // facets {
+                         //   'Component Type' {
+                         //     terms {
+                         //       field = grailsApplication.config.globalSearch.typingField
+                         //     }
+                         //   }
+                         // }
+                       }
+                     }
+
+          result.hits = search.response.hits
+        }
+        catch ( Exception e ) {
+          log.error("Error processing search", e);
+        }
+
       }
       else {
         result.message = "Unable to locate profile with id "+params.id;
--- CAPAlertsService/grails-app/views/profile/feed.gsp
+++ CAPAlertsService/grails-app/views/profile/feed.gsp
@@ -1,1 +1,6 @@
 feed.gsp
+
+<pre>
+${hits}
+</pre>
+
