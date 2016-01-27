15670b22a2d51a44c99cb2956cdb97f492b83dbf
Ian Ibbotson <ianibbo@gmail.com>
2016-01-27 11:19:44 +0000

    Query seems to work

--- CAPAlertsService/grails-app/controllers/capalerts/ProfileController.groovy
+++ CAPAlertsService/grails-app/controllers/capalerts/ProfileController.groovy
@@ -43,16 +43,16 @@ class ProfileController {
                          // }
 
                          query {
-                           bool {
-                             must {
-                               match_all {}
+                           bool : {
+                             must : {
+                               match_all : {}
                              }
-                             filter {
-                               geo_shape {
-                                 alertShape {
-                                   shape {
-                                     type "polygon"
-                                     coordinates [ [-109.5297,40.4554], [-109.5298,40.4556], [-109.5299,40.4556], [-109.5299,40.4554], [-109.5297,40.4554] ]
+                             filter : {
+                               geo_shape : {
+                                 alertShape : {
+                                   shape : {
+                                     type : "polygon"
+                                     coordinates : [ [-109.5297,40.4554], [-109.5298,40.4556], [-109.5299,40.4556], [-109.5299,40.4554], [-109.5297,40.4554] ]
                                    }
                                  }
                                }
