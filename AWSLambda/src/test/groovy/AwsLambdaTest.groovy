a22388db904280ee504cafb0639ec7e377a123f5
Ian Ibbotson <ianibbo@gmail.com>
2016-03-14 11:37:11 +0000

    Added unit test

---
+++ AWSLambda/src/test/groovy/AwsLambdaTest.groovy
@@ -0,0 +1,14 @@
+package org.gradle
+
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertNotNull
+
+class AwsLambdaTest {
+
+    @Test public void initialTest() {
+        assertEquals(1,1)
+    }
+
+}
