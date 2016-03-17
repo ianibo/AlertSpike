4a8814a15f6d895fb8d99faaef399f08c4464d71
Ian Ibbotson <ianibbo@gmail.com>
2016-03-17 11:14:39 +0000

    Working on node version of lambda client

---
+++ AWSLambdaNode/FindSubsForAlert.js
@@ -0,0 +1,71 @@
+var http = require('http');
+
+/**
+ * See https://nodejs.org/api/http.html#http_http_request_options_callback
+ */
+exports.handler = function(event, context) {
+
+    var shapeCoordinates = [[[]]];
+
+    var postData = querystring.stringify({
+                     "from":0,
+                     "size":1000,
+                     "query":{
+                       "bool": {
+                         "must": {
+                           "match_all": {}
+                         },
+                         "filter": {
+                             "geo_shape": {
+                               "subshape": {
+                                 "shape": {
+                                   "type": "polygon",
+                                   "coordinates" : shapeCoordinates
+                                 },
+                                 relation:'intersects'
+                               }
+                             }
+                           }
+                         }
+                       },
+                       "sort":{
+                         "recid":{order:'asc'}
+                       }
+
+    });
+
+    var options = {
+      hostname: 'ce.semweb.co',
+      port: 80,
+      path: '/es/alertssubscriptions/_search',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': postData.length
+      }
+    };
+
+
+    var req = http.request(options, function(res) {
+        var body = '';
+        console.log('Status:', res.statusCode);
+        console.log('Headers:', JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+            body += chunk;
+        });
+        res.on('end', function() {
+            console.log('Successfully processed HTTPS response');
+            // If we know it's JSON, parse it
+            if (res.headers['content-type'] === 'application/json') {
+                body = JSON.parse(body);
+            }
+            context.succeed(body);
+        });
+    });
+
+    req.on('error', context.fail);
+
+    req.write(postData);
+    req.end();
+};
